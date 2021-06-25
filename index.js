c9f95f58485cfbb84316f6ced2d2df80eb82811f
tarang <tarangsinghal2@gmail.com>
2021-06-25 13:37:04 +0530

    Finalising deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/api', slotRoutes);
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 
