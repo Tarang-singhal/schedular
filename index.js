0ec036edd68d7ede640faac38c8d747586b5ed36
tarang <tarangsinghal2@gmail.com>
2021-06-25 13:48:07 +0530

    Finalising deploy

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'build')));
 
 //Routes
 app.use('/api', teacherRoutes);
 app.use('/api', slotRoutes);
 
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static("client/build"));
+  app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
 
 //PORT
 const PORT = process.env.PORT || 3001;
